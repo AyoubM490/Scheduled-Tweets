a082ac4bf1ec3e71a827f03ba58b801215d487a6
ayoub <chahayb563@gmail.com>
2022-07-20 23:36:21 +0100

    Add registrations and sessions controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # GET /about
-  get "about-us", to: "about#index", as: :about
+  get "about", to: "about#index", as: :about
+
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 
   root to: "main#index"
 end
