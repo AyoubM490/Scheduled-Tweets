27188ded05619f96f18249179ce8d00bffb460ee
ayoub <chahayb563@gmail.com>
2022-07-26 12:14:47 +0100

    add token and token secret

--- app/models/twitter_account.rb
+++ app/models/twitter_account.rb
@@ -8,8 +8,8 @@ class TwitterAccount < ApplicationRecord
     Twitter::REST::Client.new do |config|
       config.consumer_key = Rails.application.credentials.dig(:twitter, :api_key)
       config.consumer_secret = Rails.application.credentials.dig(:twitter, :api_secret)
-      config.access_token = token
-      config.access_token_secret = secret
+      config.access_token = "1550053844188536833-MfSQGVnNZnvzLW6V49QyX6CpeFd0fp"
+      config.access_token_secret = "N5TXrOd2HGv8rEBCTREc16mpLiqdzgu0E8fZYD0tZ2FRP"
     end
   end
 end
